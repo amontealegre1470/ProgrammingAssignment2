541309b7b9ccd75f7e5dc86f516af6e0dfe95789
amontealegre1470 <a.montealegre1470@uniandes.edu.co>
2015-03-22 15:47:05 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -18,8 +15,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         
